aa5387e91a7347b69417cb35572c3122e72c547a
PurpleDev <65060124+PurpleDevAu@users.noreply.github.com>
2020-05-09 07:16:25 +0100

    Changes to the gui

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from tkinter import Tk
 from tkinter import Button
 from tkinter import Label
@@ -10,6 +11,7 @@ import csv
 import logging
 import struct
 import threading
+import time
 import statistics
 import sys
 
@@ -17,13 +19,17 @@ NETWORK_KEY= [0xb9, 0xa5, 0x21, 0xfb, 0xbd, 0x72, 0xc3, 0x45]
 
 counter = 0
 fileName = None
+lastRead = time.time()
+today = datetime.now().strftime("%Y%m%d%H%M")
+
 
 def on_data(data):
-    global fileName, heartRateLabel
+    global fileName, heartRateLabel, connectionIndicator, lastRead
     
-    heartrate = data[7]
-    
+    heartrate = data[7]    
     heartRateLabel.configure(text=heartrate)
+    connectionIndicator.configure(bg = "green")
+    lastRead = time.time()
        
     if fileName is None:
         pass
@@ -50,22 +56,14 @@ def createNode():
     finally:
         node.stop()
 
-def on_start():
-    global fileName, counter, window
-      if fileName is None:
-        pass
-    else:
-        fileName = "recording_" + str(counter)+".csv"
+def on_clicked():
+    global fileName, counter, today, window, control_Button
+    if fileName is None:
+        fileName = f"recording_{today}_{counter}.csv"
         counter += 1
         recording_Label = Label(window, text=fileName)
         recording_Label.grid(column = 0, row=(counter*2))
-    
-    
-    
-def on_stop():
-    global fileName, window
-    if fileName is None:
-        pass
+        control_Button.configure(text = "stop")
     else:
         lastFile = fileName           
         fileName = None
@@ -78,8 +76,8 @@ def on_stop():
         reads.pop()
         reads = list(map(int, reads))
         
-        mean = statistics.mean(reads)
-        standardDeviation = statistics.stdev(reads)
+        mean = round(statistics.mean(reads),1)
+        standardDeviation = round(statistics.stdev(reads),1)
         maxReading = max(reads)
         minReading = min(reads)
         
@@ -88,29 +86,43 @@ def on_stop():
         file.write("\n"+stats)
         
         recording_Label = Label(window, text=stats)
-        recording_Label.grid(column = 0, row=((counter*2)+1))
-    
+        recording_Label.grid(column = 0, row=((counter*2)+1), columnspan=4)       
+       
+        control_Button.configure(text = "start")
+ 
+def connectionChecker(): 
+    global lastRead, connectionIndicator
+    while(True):
+        now = time.time()
+        if(now - lastRead > 1):
+            connectionIndicator.configure(bg = "red")
 
 def createGui():
+    global window, heartRateLabel, connectionIndicator, control_Button
+
     init_Thread = threading.Thread(
         target = createNode
         )
     init_Thread.start()
     
-    global window, heartRateLabel
+    activeConnection_Thread = threading.Thread(
+        target = connectionChecker
+        )
+    activeConnection_Thread.start()
     
     window = Tk()
     window.title("Heart Rate Recorder")
     
     heartRateLabel = Label(window, text='0')
     heartRateLabel.grid(column = 2, row=0)
+
+    connectionIndicator = Button(window, text="connection", bg = "red")
+    connectionIndicator.grid(column=0, row=0, columnspan=2)
             
-    gentle_StartButton = Button(window, text="start", command=on_start)
-    gentle_StartButton.grid(column=0, row=1)
-    
-    gentle_StopButton = Button(window, text="stop", command=on_stop)
-    gentle_StopButton.grid(column=1, row=1)
-    
+    control_Button = Button(window, text="start", command=on_clicked)
+    control_Button.grid(column=0, row=1)
+
+
     window.mainloop()
 
 createGui()
